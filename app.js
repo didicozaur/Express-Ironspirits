bc9b04b85e6f7c771aef89a04fe7bca319c5a5d4
didicozaur <moisin.diana@yahoo.com>
2022-04-20 13:15:11 +0200

    handlebars buit-in helpers

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
 const express = require("express");
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
 app.use(express.static("public"));
 
-app.get("/about", (req, res, next) => {
-  // console.log(req.url)
-  console.log("a request on the ABOUT page was received... ");
-
-  res.sendFile(__dirname + "/views/About.html");
+app.get("/home", (req, res, next) => {
+  res.render("home");
 });
 
 app.get("/contact", (req, res, next) => {
-  res.sendFile(__dirname + "/views/contact.html");
+  res.render("contact");
+});
+
+app.get("/about", (req, res, next) => {
+  res.render("About");
+});
+
+app.get("/limoncello", (req, res, next) => {
+  // res.render("view", info);
+
+  const data = {
+    title: "Limoncello",
+    price: 20,
+    imageFile: "limoncello.jpg",
+  };
+
+  res.render("product", data);
+});
+
+app.get("/single-malt", (req, res, next) => {
+  // res.render("view", info);
+
+  const data = {
+    title: "Single Malt",
+    price: 50,
+    imageFile: "single-malt.jpg",
+  };
+
+  res.render("product", data);
+});
+
+app.get("/tequilla", (req, res, next) => {
+  // res.render("view", info);
+
+  const data = {
+    title: "Tequilla",
+    price: 35,
+    imageFile: "tequila.jpg",
+    stores: ["Online", "Albacete", "Freiburg", "Amsterdam"],
+  };
+
+  res.render("product", data);
 });
 
 app.listen(3001, () => {
