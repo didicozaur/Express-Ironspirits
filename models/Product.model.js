adff8881fb0e5e7b492f1f79ba7a5412c081c41e
didicozaur <moisin.diana@yahoo.com>
2022-04-22 11:21:47 +0200

    created Product model

---
+++ models/Product.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  price: {
+    type: Number,
+    min: 1,
+  },
+  hasStock: {
+    type: Boolean,
+    default: true,
+  },
+  tags: {
+    type: [String],
+    enum: ["spirits", "drink", "italian", "japanise", "best-sellers"],
+  },
+  imgFile: {
+    type: String,
+    default: "../images/default.jpg",
+  },
+  store: {
+    type: String,
+    enum: ["online", "germany", "colombia"],
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
